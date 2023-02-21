4cca41f3e57a0e9e4a7f176573cc98e8fc6a2733
anvy <0073av@gmail.com>
2023-02-21 16:34:59 +0800

    load more button

--- app-root/src/app/grid/grid.component.ts
+++ app-root/src/app/grid/grid.component.ts
@@ -8,19 +8,39 @@ import { Component, OnInit } from '@angular/core';
 export class GridComponent implements OnInit {
   menu: string[] = ['all', 'favorite', 'menuPoint'];
   menuPoint: number = 0;
-  preview_emoji: string = '';
+  previewEmoji: string = '';
+  srcs: any[] = [''];
+  amount_plus: number = 50;
+  amount_state: number = 50;
+  emojis: any = {}
+
   ngOnInit() {
     fetch('https://api.github.com/emojis')
       .then((r) => r.json())
       .then((data) => {
-        const first_e = Object.keys(data)[0]
-        console.log(first_e)
-        this.preview_emoji = data[first_e];
+        this.emojis = data
+        const allKeys = Object.keys(data);
+        const partKeys = allKeys.splice(0, this.amount_plus);
+        this.srcs = partKeys.map((e) => {
+          return { name: e, link: data[e] };
+        });
+
       });
   }
 
   pickPoint(i: number) {
     this.menuPoint = i;
     console.log(this.menuPoint);
+  }
+
+  loadMore(){
+    const allKeys = Object.keys(this.emojis);
+    const partKeys = allKeys.splice(this.amount_state, this.amount_plus);
+    const more_srcs = partKeys.map((e) => {
+      return { name: e, link: this.emojis[e] };
+    });
+    this.srcs = [...this.srcs, ...more_srcs]
+
+    this.amount_state += this.amount_plus
   }
 }
