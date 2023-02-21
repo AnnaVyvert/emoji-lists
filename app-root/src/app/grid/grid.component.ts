dda023178cf00295fd0477e1c3162413e98466ae
anvy <0073av@gmail.com>
2023-02-21 14:57:06 +0800

    pick point

--- app-root/src/app/grid/grid.component.ts
+++ app-root/src/app/grid/grid.component.ts
@@ -1,17 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'grid-component',
   templateUrl: './grid.component.html',
-  styleUrls: ['./grid.component.css']
+  styleUrls: ['./grid.component.css'],
 })
-export class GridComponent {
-  menu: string[] = ['all','favorite','menuPoint']
-  menuPoint: number = 0
-  
-  pickPoint(i: number){
-    this.menuPoint = i
+export class GridComponent implements OnInit {
+  menu: string[] = ['all', 'favorite', 'menuPoint'];
+  menuPoint: number = 0;
+  preview_emoji: string = '';
+  ngOnInit() {
+    fetch('https://api.github.com/emojis')
+      .then((r) => r.json())
+      .then((data) => {
+        const first_e = Object.keys(data)[0]
+        console.log(first_e)
+        this.preview_emoji = data[first_e];
+      });
+  }
+
+  pickPoint(i: number) {
+    this.menuPoint = i;
     console.log(this.menuPoint);
-    
   }
 }
