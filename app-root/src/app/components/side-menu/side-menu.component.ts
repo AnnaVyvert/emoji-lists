508fbe54c868eed563e50c08b797671dad3fc2da
anvy <0073av@gmail.com>
2023-02-23 16:32:33 +0800

    complete side menu

--- app-root/src/app/components/side-menu/side-menu.component.ts
+++ app-root/src/app/components/side-menu/side-menu.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'side-menu',
@@ -8,30 +7,27 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./side-menu.component.sass'],
 })
 export class SideMenuComponent {
-
   menu: string[] = ['all', 'favorite', 'deleted'];
 
-  activePoint: string = 'all'
+  activePoint: string = 'all';
 
   constructor(private activatedRoute: ActivatedRoute) {
     this.activatedRoute.params.subscribe((entities: any) => {
-      this.activePoint = entities.listname
-      
-    }) 
+      this.activePoint = entities.listname;
+    });
   }
 
-  isAuth: boolean = window.localStorage['access_token']
+  isAuth: boolean = window.localStorage['access_token'];
 
-  openMenu(bool :boolean) {
-    const sideNav = document.querySelector('.side-nav')
-    const sideToNav = document.querySelector('.side-to-nav')
-    if(bool){
-      sideNav?.setAttribute('style', 'width: 16em')
-      sideToNav?.setAttribute('style', 'width: 100%')
-    }else{
-      sideNav?.setAttribute('style', 'width: 0')
-      sideToNav?.setAttribute('style', 'width: 0')
+  openMenu(bool: boolean) {
+    const sideNav = document.querySelector('.side-nav');
+    const sideToNav = document.querySelector('.side-to-nav');
+    if (bool) {
+      sideNav?.setAttribute('style', 'width: 16em');
+      sideToNav?.setAttribute('style', 'width: 100%');
+    } else {
+      sideNav?.setAttribute('style', 'width: 0');
+      sideToNav?.setAttribute('style', 'width: 0');
     }
   }
-
 }
